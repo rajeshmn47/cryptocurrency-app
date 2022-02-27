b65918348e4c2a420e2a387f5fe5456b4e6295a2
rajeshmn47 <rajeshmn47@gmail.com>
2022-02-27 20:38:36 +0530

    second commit

--- src/components/Cryptodetails.js
+++ src/components/Cryptodetails.js
@@ -1,9 +1,18 @@
 import { useEffect,useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from 'axios'
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import { Line } from "react-chartjs-2";
 
 export const Cryptodetails=()=>{
     const[currencydetails,setCurencydetails]=useState()
+    const[historicaldata,setHistoricaldata]=useState([])
+    const [days, setDays] = useState(1);
     const id=useParams()
     console.log(id)
     useEffect(()=>{
@@ -26,14 +35,64 @@ export const Cryptodetails=()=>{
         }).catch(function (error) {
             console.error(error);
         });
+        var options = {
+          method: 'GET',
+          url: 'https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-historical-data',
+          params: {
+            pair_ID:id.id,
+            date_from: '26022021',
+            date_to: '19092021',
+            lang_ID: '1',
+            time_utc_offset: '28800',
+            interval: 'day'
+          },
+          headers: {
+            'x-rapidapi-host': 'investing-cryptocurrency-markets.p.rapidapi.com',
+            'x-rapidapi-key': '3ddef92f6emsh8301b1a8e1fd478p15bb8bjsnd0bb5446cadc'
+          }
+        };
+        
+        axios.request(options).then(function (response) {
+          console.log(response.data.data[0].screen_data.data);
+          setHistoricaldata(response.data.data[0].screen_data.data)
+        }).catch(function (error) {
+          console.error(error);
+        });
     },[])
     return(
       <>
-      {currencydetails&&currencydetails.map((c)=>
+      {currencydetails?(currencydetails.map((c)=>
         <>
         <h1>   {c.search_main_longtext}</h1>
-        </>)}
-        </>
+        <Line
+         data={{
+          labels: historicaldata.map((coin) => {
+            let date =coin.date
+        
+            return date.toLocaleDateString();
+          }),
+
+          datasets: [
+            {
+              data: historicaldata.map((coin) => coin.perc_chg),
+              label: `Price ( Past2 Days ) in currency`,
+              borderColor: "#EEBC1D",
+            },
+          ],
+        }}
+        options={{
+          elements: {
+            point: {
+              radius: 1,
+            },
+          },
+        }}
+        />
+        </>)
+      ):<CircularProgress   style={{ color: "gold" }}
+      size={250}
+      thickness={1}/>}
+      </>
     )
 }
 export default Cryptodetails
